432224dab4e169e9fa4dc3679419e3a8d7bc7b0e
artbels <artbels@gmail.com>
2023-07-10 11:29:53 +0300

    unlisted changes

--- translit.js
+++ translit.js
@@ -2,87 +2,125 @@
  * Cyr2lat translit
  * @ artbels
  * artbels@gmail.com
- * 2016
- * ver 1.0.0
+ * 2023
+ * ver 2.0.0
  *
  */
 
-;(function () {
-  var Translit = this.Translit = function (input) {
+;(function() {
+  var Translit = this.Translit = function(str) {
+    const reOtherSymbols = /[^a-z0-9\-_]/gi
 
-    if(!input) return
+    const replSymb = translit(str)
+    .replace(/[\/\\]/g, '_')
+    .replace(reOtherSymbols, '-')
 
-    if(typeof input === 'number') input = input.toString()
+    const replUnnecDelims = removeUnnecessaryDelims(replSymb)
 
-    if(typeof input !== 'string') return 
+    return replUnnecDelims.toLowerCase()
 
-    var letterMap = {
-      '/': '_',
-      '\\': '_',
-      "'": '',
-      'а': 'a',
-      'б': 'b',
-      'в': 'v',
-      'г': 'g',
-      'д': 'd',
-      'е': 'e',
-      'ж': 'zh',
-      'з': 'z',
-      'и': 'i',
-      'й': 'y',
-      'к': 'k',
-      'л': 'l',
-      'м': 'm',
-      'н': 'n',
-      'о': 'o',
-      'п': 'p',
-      'р': 'r',
-      'с': 's',
-      'т': 't',
-      'у': 'u',
-      'ф': 'f',
-      'х': 'kh',
-      'ц': 'ts',
-      'ч': 'ch',
-      'ш': 'sh',
-      'щ': 'sch',
-      'ы': 'i',
-      'ь': '',
-      'ъ': '',
-      'э': 'e',
-      'ю': 'yu',
-      'я': 'ya',
-      'ё': 'e',
-      'є': 'e',
-      'і': 'i',
-      'ї': 'yi',
-      'ґ': 'g',
-      '+': '-plus'
-    }
-
-    var reOtherSymbols = /[^a-z0-9\-_]/gi
-
-    var replLetters = input.replace(/київ/i, 'kyiv').split('').map(function (char) {
-      char = char.toLowerCase()
-      return (letterMap[char] !== undefined) ? letterMap[char] : char
-    }).join('')
-
-    var replSymb = replLetters.replace(reOtherSymbols, '-')
-
-    var replUnnecDelims = removeUnnecessaryDelims(replSymb)
-
-    return replUnnecDelims
-
-    function removeUnnecessaryDelims (input) {
-      return input        
+    function removeUnnecessaryDelims(input) {
+      return input
         .replace(/\-{2,}/g, '-')
         .replace(/_{2,}/g, '_')
         .replace(/[\-\_]+$/g, '')
         .replace(/^[\-\_]+/g, '')
+    }
+
+    function translit(input) {
+      input = input || ''
+
+      return input
+        .replace(/іє/g, 'ie') // Exception
+        .replace(/Іє/g, 'Ie') // Exception
+        .replace(/ія/g, 'ia') // Exception
+        .replace(/Ія/g, 'Ia') // Exception
+        .replace(/зг/g, 'zgh') // Exception
+        .replace(/Зг/g, 'Zgh') // Exception
+        .replace(/ьо/g, 'io') // Exception
+        .replace(/а/g, 'a')
+        .replace(/б/g, 'b')
+        .replace(/в/g, 'v')
+        .replace(/г/g, 'h')
+        .replace(/ґ/g, 'g')
+        .replace(/д/g, 'd')
+        .replace(/е/g, 'e')
+        .replace(/(^|\s)є/g, '$1ye')
+        .replace(/є/g, 'ie')
+        .replace(/ж/g, 'zh')
+        .replace(/з/g, 'z')
+        .replace(/и/g, 'y')
+        .replace(/і/g, 'i')
+        .replace(/(^|\s)ї/g, '$1yi')
+        .replace(/ї/g, 'i')
+        .replace(/(^|\s)й/g, '$1y')
+        .replace(/й/g, 'i')
+        .replace(/к/g, 'k')
+        .replace(/л/g, 'l')
+        .replace(/м/g, 'm')
+        .replace(/н/g, 'n')
+        .replace(/о/g, 'o')
+        .replace(/п/g, 'p')
+        .replace(/р/g, 'r')
+        .replace(/с/g, 's')
+        .replace(/т/g, 't')
+        .replace(/у/g, 'u')
+        .replace(/ф/g, 'f')
+        .replace(/х/g, 'kh')
+        .replace(/ц/g, 'ts')
+        .replace(/ч/g, 'ch')
+        .replace(/ш/g, 'sh')
+        .replace(/щ/g, 'sch')
+        .replace(/ь/g, '')
+        .replace(/(^|\s)ю/g, '$1yu')
+        .replace(/ю/g, 'iu')
+        .replace(/(^|\s)я/g, '$1ya')
+        .replace(/я/g, 'ia')
+        .replace(/А/g, 'A')
+        .replace(/Б/g, 'B')
+        .replace(/В/g, 'V')
+        .replace(/Г/g, 'H')
+        .replace(/Ґ/g, 'G')
+        .replace(/Д/g, 'D')
+        .replace(/Е/g, 'E')
+        .replace(/(^|\s)Є/g, '$1Ye')
+        .replace(/Є/g, 'Ie')
+        .replace(/Ж/g, 'Zh')
+        .replace(/З/g, 'Z')
+        .replace(/И/g, 'Y')
+        .replace(/І/g, 'I')
+        .replace(/(^|\s)Ї/g, '$1Yi')
+        .replace(/Ї/g, 'I')
+        .replace(/(^|\s)Й/g, '$1Y')
+        .replace(/Й/g, 'I')
+        .replace(/К/g, 'K')
+        .replace(/Л/g, 'L')
+        .replace(/М/g, 'M')
+        .replace(/Н/g, 'N')
+        .replace(/О/g, 'O')
+        .replace(/П/g, 'P')
+        .replace(/Р/g, 'R')
+        .replace(/С/g, 'S')
+        .replace(/Т/g, 'T')
+        .replace(/У/g, 'U')
+        .replace(/Ф/g, 'F')
+        .replace(/Х/g, 'Kh')
+        .replace(/Ц/g, 'Ts')
+        .replace(/Ч/g, 'Ch')
+        .replace(/Ш/g, 'Sh')
+        .replace(/Щ/g, 'Sch')
+        .replace(/Ь/g, '')
+        .replace(/(^|\s)Ю/g, '$1Yu')
+        .replace(/Ю/g, 'Iu')
+        .replace(/(^|\s)Я/g, '$1Ya')
+        .replace(/Я/g, 'Ia')
+        .replace(/'/g, '')
+        .replace(/’/g, '')
+        .replace(/`/g, '')
     }
   }
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Translit
   }
-})()
+})();
